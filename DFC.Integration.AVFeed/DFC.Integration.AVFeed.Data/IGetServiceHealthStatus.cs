06e57edf8feef19aeb3abe298e0fd465c8f81103
Dinesh Tripatthi <dinesh.tripathi@aaditricorp.onmicrosoft.com>
2018-02-14 09:20:24 +0000

    As new Story created to will mark this branch as close

---
+++ DFC.Integration.AVFeed/DFC.Integration.AVFeed.Data/IGetServiceHealthStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFC.Integration.AVFeed.Data
+{
+    public interface IGetServiceHealthStatus
+    {
+
+        Task<dynamic> GetHealthStatusInfo();
+    }
+}
