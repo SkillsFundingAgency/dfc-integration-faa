4c492b8fb01d907e0b176504de18665af589eaed
Dinesh Tripatthi <dinesh.tripathi@aaditricorp.onmicrosoft.com>
2018-02-14 09:23:14 +0000

    WIP

--- DFC.Integration.AVFeed/DFC.Integration.AVFeed.Function.GetServiceHealthStatus/GetServiceHealthStatus.cs
+++ DFC.Integration.AVFeed/DFC.Integration.AVFeed.Function.GetServiceHealthStatus/GetServiceHealthStatus.cs
@@ -1,8 +1,5 @@
 ﻿using DFC.Integration.AVFeed.Data;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace DFC.Integration.AVFeed.Function.GetServiceHealthStatus
