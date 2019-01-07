af5a87ac84b3635840589347486b6764a2f43eae
gurney <Gurney@as-tech.co.uk>
2019-01-07 08:57:56 +0000

    Added test to check that we don't cal the service more than the number of totalpages reported.

--- DFC.Integration.AVFeed/DFC.Integration.AVFeed.Service.AVAPI/AVAPIService.cs
+++ DFC.Integration.AVFeed/DFC.Integration.AVFeed.Service.AVAPI/AVAPIService.cs
@@ -65,7 +65,7 @@ namespace DFC.Integration.AVFeed.Service
                 if (apprenticeshipVacancySummaryResponse?.Results != null)
                     avSummary.AddRange(apprenticeshipVacancySummaryResponse.Results);
 
-                //stop when there are no more pages or we have more then multiple supplier
+                    //stop when there are no more pages or we have more then multiple supplier
                     if (apprenticeshipVacancySummaryResponse?.TotalPages <= pageNumber ||
                         avSummary.Select(v => v.TrainingProviderName).Distinct().Count() > 1)
                         break;
