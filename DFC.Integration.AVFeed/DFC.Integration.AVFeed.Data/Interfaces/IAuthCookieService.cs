38c105e47e1b89aac60e6e1f4a88348e88185a06
Trevor Kapswarah <trevk15@yahoo.co.uk>
2019-01-09 23:01:26 +0000

    WIP - Before Merging into Gurney

---
+++ DFC.Integration.AVFeed/DFC.Integration.AVFeed.Data/Interfaces/IAuthCookieService.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DFC.Integration.AVFeed.Data.Interfaces
+{
+    public interface IAuthCookieService
+    {
+        Task<string> GetAuthCookieAsync();
+    }
+}
