ec712935c537ab93d243ddc7c70427b2bb87b0b0
gurney <Gurney@as-tech.co.uk>
2019-01-10 14:21:59 +0000

    Added unit test for ClearRecycleBin

---
+++ DFC.Integration.AVFeed/DFC.Intergration.AVfeed.Function.ClearRecycleBinTests/DeleteRecycleBinTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DFC.Integration.AVFeed.Data.Interfaces;
+using DFC.Integration.AVFeed.Function.ClearRecycleBin;
+using FakeItEasy;
+using Xunit;
+
+namespace DFC.Integration.AVfeed.Function.ClearRecycleBinTests
+{
+    public class ClearRecycleBinTests
+    {
+        [Fact]
+        public async void ClearRecycleBinAsyncTest()
+        {
+            //Setup
+            var fakeCustomSitefinityAPIs = A.Fake<ICustomSitefinityAPIs>();
+            var fakeCustomApiContextService = A.Fake<ICustomApiContextService>();
+            var fakeTokenClient = A.Fake<ITokenClient>();
+            var fakeApplicationLogger = A.Fake<IApplicationLogger>();
+            var fakeAuditService = A.Fake<IAuditService>();
+
+            //A.CallTo(() => fakeCustomApiContextService.ClearAVsRecycleBinAsync()).
+         
+            var clearRecycleBin = new ClearRecycleBin(fakeCustomSitefinityAPIs, fakeApplicationLogger, fakeAuditService, fakeCustomApiContextService);
+
+            //Act
+            await clearRecycleBin.ClearRecycleBinAsync();
+
+            //Asserts
+            A.CallTo(() => fakeCustomApiContextService.ClearAVsRecycleBinAsync(A<int>._)).MustHaveHappened();
+
+        }
+    }
+}
